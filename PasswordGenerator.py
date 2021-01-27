d09b56640106c8d19e23c40b9c0f612cad69b019
ManosVourtsanis <manos.vou7@hotmail.com>
2021-01-27 19:57:49 +0200

    entrance commit2

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,17 +1,27 @@
-
 import time
+import subprocess
+import os
 
 
+def entrance():
+    print("_______WELCOME TO PASSWORD GENERATOR_______")
+    time.sleep(1)
+    
 
-
-def menu():
-    print("_______WELCOME_______")
-    time.sleep(3)
-
-
-def main():
-    menu()
+    if os.stat("savedPin.txt").st_size == 0:
+        f = open("savedPin.txt", "w")
+        newPin = input("Please type your new 4-digit pin: ")
+        f.write(newPin)
+        f.close()
+        subprocess.check_call(["attrib","+H","savedPin.txt"])
+    else:
+        savedPin = open('savedPin.txt', 'r').read()
+        pin = input("Insert your pin to access the app: ")
+        if pin == savedPin:
+            print("You have access")
+        else:
+            print("Wrong")
 
 
 if __name__ == "__main__":
-    main()
+    entrance()
